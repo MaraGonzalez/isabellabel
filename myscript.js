4c6000788312c66b6d65f0682cb8cb0e3af89393
MaraGonzalez <112019954+MaraGonzalez@users.noreply.github.com>
2023-01-19 11:32:42 -0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,52 @@
+function myFunction1() {
+    alert("Hola Isa, vamos a comenzar el juego. ¡Apreta el botón 4!");
+  }
+
+  function myFunction2() {
+    alert("Ya lo vamos teniendo!!!! Ahora el botón 10.");
+  }
+
+
+  function myFunction3() {
+    alert("Nos falta poquitotototo. Tocá el botón 8.");
+  }
+
+
+
+  function myFunction4() {
+    alert("¡Muy bien! Ahora el Botón 2.");
+  }
+
+
+
+  function myFunction5() {
+    alert("No nos olvidemos de tocar el 9, tocá el botón 9");
+  }
+
+
+  function myFunction6() {
+    alert("Uno más y no jodemos más: anda pal 7");
+  }
+
+
+  function myFunction7() {
+    alert("Si seguiste todos los paso correctamente y llegaste hasta aca. Anda al que dice NO TOCAR!!!!");
+  }
+
+
+
+  function myFunction8() {
+    alert("Siguiente botón: 6");
+  }
+
+  function myFunction9() {
+    alert("Iujuuuuuuuuuuuuuu. Ahora el botón 3");
+  }
+
+  function myFunction10() {
+    alert("TURUTURUTURU. Tocá el botón 5");
+  }
+
+  function myFunction0() {
+    alert("El 20 de febrero no planees nada. Tenemos una cita vos, el kevo y yo. Lugar: TEMAIKEN! Reclama tu entrada con la mejor madrina del mundo y con el tio kevo");
+  }
